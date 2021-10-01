c7ad6cfbcaaf8018584304430574f6027b567cd2
mlamont8 <mlamont8@hotmail.com>
2021-10-01 10:08:15 -0400

    added images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,185 +1,241 @@
 import * as React from "react"
-import "../styles/global.scss"
+import Fade from "react-reveal/Fade"
+import Layout from "../components/layout"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+const IndexPage = ({data}) => {
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
-// markup
-const IndexPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+  return(
+    <Layout>
+            <section className="section mission" style={{ padding: "5rem 1.5rem" }}>
+        <div className="container">
+          <h2
+            className="subtitle is-size-3"
+            style={{
+              borderLeft: "solid",
+              borderColor: "#003f99",
+              paddingLeft: "15px",
+            }}
           >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+            ...to prepare young people to make ethical and moral choices over
+            their lifetimes by instilling in them the values of the Scout Oath
+            and Law.{" "}
+          </h2>
+        </div>
+      </section>
+      <section className="intro">
+        <div className="columns">
+          <div className="column is-paddingless">
+            <div className="has-text-centered  leftInnerColumn has-background-black">
+              <h1 className="title has-text-warning is-family-secondary">
+                Bethel Cubscout Pack 723
+              </h1>
+              <h2 className="subtitle has-text-warning">Greensboro, NC</h2>
+              <p className="is-size-6">
+                Pack 723 provides well-rounded and fun activities with true and
+                lasting value for boys and their families in Greensboro. The
+                activities are based on more than just having good times; they
+                offer the satisfaction of meeting challenges, the confidence
+                created by accomplishing goals and the camaraderie of lasting
+                friendships. Boys have a blast completing adventures at the same
+                time they achieve the aims of scouting – character development,
+                participatory citizenship, personal fitness, outdoor skills, and
+                leadership and life skills. Many former members of Pack 723 have
+                transitioned into Boy Scouts, Eagle Scouts and roles of
+                leadership and influencers throughout the community.We invite
+                you and your son to join us!
+              </p>
+            </div>
+          </div>
+          <div
+            className="column is-paddingless intro-image-container"
+            style={{ display: `flex`, alignItems: "center" }}
+            data-sal="fade"
+            data-sal-easing="ease"
+            data-sal-duration="1000"
+          >
+            <Img
+              fluid={data.trailer.childImageSharp.fluid}
+              className="intro-image"
+              alt="Pack 723 on an activity"
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className="intro">
+        <div className="columns columns-alt">
+          <div
+            className="column is-paddingless intro-image-container"
+            id="intro-left-image-container"
+            data-sal="fade"
+            data-sal-easing="ease"
+            data-sal-duration="500"
+          >
+            <Img
+              fluid={data.sub.childImageSharp.fluid}
+              className="intro-image"
+              alt="Scouts visit the USS Clamagore"
+            />
+          </div>
+          <div className="column is-paddingless">
+            <div className="has-text-centered rightInnerColumn has-background-black">
+              <h1 className="title has-text-warning">What is Scouting?</h1>
+              <p>
+                Scouting compliments the school and the family, filling needs
+                when necessary. Scouting develops self-knowledge and the need to
+                explore, to discover, and to want to know. Scouts discover the
+                world beyond the classroom, tapping the skills of others to
+                learn and to become well-rounded people.
+              </p>
+
+              <h2
+                className="subtitle has-text-warning"
+                style={{ paddingTop: "30px" }}
               >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+                Fun with a Purpose
+              </h2>
+              <p>
+                Through recreation, Scouting achieves its purpose of helping
+                young people develop physically, intellectually, socially, and
+                spiritually. Scouting is all about building confidence and
+                self-esteem, learning important life skills and leadership
+                skills, team building, outdoor adventure, education, and fun!
+                Scouts learn how to make good choices and to take responsibility
+                for their actions so that they are prepared for their adult life
+                as independent persons.
+              </p>
+              <h2
+                className="subtitle has-text-warning"
+                style={{ paddingTop: "30px" }}
+              >
+                Principles to Live By
+              </h2>
+              <p>
+                Scouting's Principles describe a simple code of living to which
+                all Scouts make a personal commitment through the Scout Promise
+                and Law. Scouting helps Scouts learn how to carry out their
+                commitment in everyday life.{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="section scout-law">
+        <Fade>
+          <h2 className="subtitle">A Scout is...</h2>
+        </Fade>
+
+        <div className="grid has-text-centered" style={{ width: "100%" }}>
+          <content className="a content grid-item">
+            <ul>
+              <Fade left cascade>
+                <li>Trustworthy</li>
+              </Fade>
+              <Fade left cascade delay={1000}>
+                <li>Loyal</li>
+              </Fade>
+              <Fade left cascade delay={2000}>
+                <li>Helpful</li>
+              </Fade>
+            </ul>
+          </content>
+          <div className="b grid-item">
+            <Img
+              fluid={data.campfire.childImageSharp.fluid}
+              alt="Campfire at the fall family campout"
+            />
+          </div>
+          <content className="c content grid-item">
+            <ul>
+              <Fade left cascade>
+                <li>Friendly</li>
+              </Fade>
+              <Fade left cascade delay={1000}>
+                <li>Courteous</li>
+              </Fade>
+              <Fade left cascade delay={2000}>
+                <li>Kind</li>
+              </Fade>
+            </ul>
+          </content>
+          <div className="d grid-item">
+            <Img
+              fluid={data.crafts.childImageSharp.fluid}
+              alt="Scouts activities include crafts and art"
+            />
+          </div>
+          <content className="e content grid-item">
+            <ul>
+              <Fade left cascade>
+                <li>Obedient</li>
+              </Fade>
+              <Fade left cascade delay={1000}>
+                <li>Cheerful</li>
+              </Fade>
+              <Fade left cascade delay={2000}>
+                <li>Thrifty</li>
+              </Fade>
+            </ul>
+          </content>
+          <div className="f grid-item">
+            <Img
+              fluid={data.ship.childImageSharp.fluid}
+              alt="Fall family campout 2019"
+            />
+          </div>
+          <content className="g content grid-item">
+            <ul>
+              <Fade left cascade>
+                <li>Brave</li>
+              </Fade>
+              <Fade left cascade delay={1000}>
+                <li>Clean</li>
+              </Fade>
+              <Fade left cascade delay={2000}>
+                <li>Reverent</li>
+              </Fade>
+            </ul>
+          </content>
+          <div className="h grid-item">
+            <Img
+              fluid={data.walking.childImageSharp.fluid}
+              alt="Scouts walking on a camping trip"
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className="hero is-medium is-primary is-bold">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title has-text-white">Scout Oath</h1>
+
+            <div className="columns">
+              <div className="column is-one-third">
+                <Img
+                  fluid={data.pledge.childImageSharp.fluid}
+                  alt="Scouts on the USS Yorktown"
+                />
+              </div>
+              <div className="column oath">
+                <Fade bottom delay={1000}>
+                  <p className="is-size-4">
+                    "On my honor I will do my best; To do my duty to God and my
+                    country and to obey the Scout Law; To help other people at
+                    all times; To keep myself physically strong, mentally awake,
+                    and morally straight."
+                  </p>
+                </Fade>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Layout>
   )
 }
+
+
 
 export default IndexPage
